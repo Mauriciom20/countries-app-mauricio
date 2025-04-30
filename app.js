f96fa99d465b2dc595779947bb811e54deadb7cb
mauricio <mauricio_moralesv@soy.sena.edu.co>
2025-04-29 20:33:24 -0500

    IMPLEMENTADO FUNCION MAP QUE MUESTRA LA LISTA DE PAISES

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import { countries } from './countries.js'
 
 console.log(countries)
+
+const container = document.querySelector('.country-card');
+
+countries.map(({ countries, capital,population }) => {
+  const div = document.createElement('div');
+  div.className = 'country';
+  div.innerHTML = `<h2>${countries}</h2><p>Capital: ${capital}</p><p>Population: ${population}</p>`;
+  container.appendChild(div);
+});
